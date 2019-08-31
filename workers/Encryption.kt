174b59ac6dd74c4c633d8504b8b5d88b945cce8e
Mohammad Foroughi <foroughi1380@gmail.com>
2019-08-31 11:02:15 +0430

    add Constructor to Enctryction.kt

--- workers/Encryption.kt
+++ workers/Encryption.kt
@@ -1,6 +1,24 @@
 package workers
 
+import java.io.File
 import java.security.PrivateKey
+import java.security.PublicKey
 
 class Encryption{
+
+    //streams
+    private val input : File
+    private val output : File
+
+    //keys
+    private val publicKey : PublicKey
+    private val privateKey : PrivateKey
+
+    constructor(input : File , output : File , publicKey: PublicKey , privateKey: PrivateKey){
+        this.input = input
+        this.output = output
+        this.publicKey = publicKey
+        this.privateKey = privateKey
+    }
+
 }
