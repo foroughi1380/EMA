98d86fff3b4fc049638c48f4ad422e6c5c1d476d
Mohammad Foroughi <foroughi1380@gmail.com>
2019-08-31 22:52:17 +0430

    add Head Reader an Fix program bug and my Bug git add workers/Encryption.kt

--- workers/Encryption.kt
+++ workers/Encryption.kt
@@ -1,17 +1,16 @@
 package workers
 
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.InputStream
-import java.io.OutputStream
+import java.io.*
 import java.nio.file.FileAlreadyExistsException
 import java.nio.file.InvalidPathException
 import java.security.PrivateKey
 import java.security.PublicKey
+import java.util.RandomAccess
 import javax.crypto.Cipher
-import javax.crypto.CipherOutputStream
+import javax.crypto.CipherInputStream
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
 import javax.naming.InvalidNameException
 import javax.naming.NoPermissionException
 
@@ -22,23 +21,24 @@ class Encryption{
     private val input : File
     private val output : File
     private val inStream : InputStream
-    private val outStream : OutputStream
+    private val outFile : File
 
     //keys
     private val publicKey : PublicKey
     private val privateKey : PrivateKey
 
-    constructor(input : File, dir_output : File, publicKey: PublicKey, privateKey: PrivateKey, outputname : String = input.name) {
+    constructor(input : File, dir_output : File, publicKey: PublicKey, privateKey: PrivateKey, output_name : String? = input.name) {
 
         /*check validation*/
         if (!input.exists()) throw FileNotFoundException("`${input.absolutePath}` is not exist.")
         if (!input.isFile) throw InvalidPathException("`${input.absolutePath}` is not a File.", null)
-        if (!input.canRead()) throw NoPermissionException("`${input.absolutePath}` : Permission Denied.")
-        if (input.name.toByteArray().size > 512 - (1 + 32 + 2) ) throw InvalidNameException("File name is large") // Head File is 512 Byte => 1 Byte to Store Version of Program , 32 Byte to store Secret Key , 2 Byte to Controller , other Byte to File name
+        if (!input.canRead()) throw NoPermissionException("`${input.absolutePath}` permission denied.")
+        if (input.name.toByteArray().size > 512 - (1 + 32 + 2 + 11) ) throw InvalidNameException("File name is large") // Head File is 512 Byte => 1 Byte to Store Version of Program , 32 Byte to store Secret Key , 2 Byte to Controller , other Byte to File name , 11 is padding
 
         if (!dir_output.exists()) throw FileAlreadyExistsException("`${dir_output.exists()}` is not exist.")
         if (!dir_output.isDirectory) throw InvalidPathException("`${dir_output.absolutePath}` is not a directory.", null)
         if (!dir_output.canWrite()) throw NoPermissionException("`${dir_output.absolutePath}` : Permission Denied.")
+
 
         /*init vars*/
         this.input = input
@@ -46,11 +46,13 @@ class Encryption{
         this.inStream = input.inputStream()
         this.publicKey = publicKey
         this.privateKey = privateKey
-        this.outStream = File("${dir_output.absolutePath}${File.separator}${outputname.split(".")[0]}.${TYPE}").outputStream()
+        this.outFile = File("${dir_output.absolutePath}${File.separator}${output_name!!.split(".")[0]}.${TYPE}")
     }
 
     //Write , Read Head
     fun writeHead() : SecretKey{
+
+
         /*
         *  Head length = 512 Byte
         *  Head is : |one Byte Version|32 Byte Secret key|2 Byte Controller|
@@ -70,23 +72,63 @@ class Encryption{
         controller[0] = (empty_byte and 0xff).toByte()
         controller[1] = (empty_byte shr 8 and 0xff).toByte()
 
-        //create head bytes
-
+        //create head byte array
         val head_bytes = arrayListOf<Byte>()
         head_bytes.add(version)
         head_bytes.addAll(key.encoded.toList())
         head_bytes.addAll(controller.toList())
         head_bytes.addAll(name_bytes.toList())
 
-        //create output writer
+
+        //start write
         val cipher = Cipher.getInstance(RSA)
         cipher.init(Cipher.ENCRYPT_MODE , publicKey)
-        val head_writer = CipherOutputStream(outStream , cipher)
-
-        //start Write
-        head_writer.write(head_bytes.toByteArray())
-
+        val ebytes = cipher.doFinal(head_bytes.toByteArray())
+        outFile.writeBytes(ebytes)
         return key
+    }
+
+    fun readHead() : HeadFile {
+        /*
+        * this method read head file
+        * one byte to version
+        * 32 byte to secret key
+        * 2 byte to controller
+        * and other to name file
+        * */
+
+
+
+        //init reader
+        val cipher = Cipher.getInstance(RSA)
+        cipher.init(Cipher.DECRYPT_MODE , privateKey)
+        var buff = ByteArray(512)
+        val head_reader = CipherInputStream(inStream , cipher)
+
+        /* read data*/
+
+        //read version
+        val buff_version = ByteArray(1)
+        head_reader.read(buff_version)
+        val version = buff_version[0]
+
+        //read secret key
+        val buff_secret_key = ByteArray(32)
+        head_reader.read(buff_secret_key)
+        val secretKey = SecretKeySpec(buff_secret_key , AES)
+
+        //read controller
+        val buff_controller = ByteArray(2)
+        head_reader.read(buff_controller)
+        val controller : Short = (buff_controller[1].toInt() and 0xFF shl 8 or (buff_controller[0].toInt() and 0xFF)).toShort()
+
+        //read file name
+        val buff_name = ByteArray(512 - (1 + 32 + 2 + controller )) // controller is empty bytes
+        head_reader.read(buff_name)
+        val name = String(buff_name)
+
+        //create and return the HeadFile
+        return HeadFile(version , secretKey , controller , name)
     }
 
     companion object{
@@ -94,5 +136,6 @@ class Encryption{
         const val AES = "AES"
         const val RSA = "RSA"
         const val TYPE = "ema"
+        data class HeadFile(val version : Byte , val key: SecretKey , val controller : Short , val name : String) // data class for store read head file
     }
 }
