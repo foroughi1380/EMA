7f7e1eb979331f7b90a968a86116379d3e3c96c5
Mohammad Foroughi <foroughi1380@gmail.com>
2019-09-02 15:57:05 +0430

    add constructor

--- workers/KeyStorage.kt
+++ workers/KeyStorage.kt
@@ -1,5 +1,25 @@
 package workers;
 
-public class KeyStorage {
+import java.io.File
+import java.lang.Exception
+import java.nio.file.NotDirectoryException
 
+class KeyStorage {
+
+    private val dir : File
+
+    constructor(dir : File){
+        //check dir
+        if (! dir.exists()) throw DirectoryNotExist("`${dir.absolutePath}` is not exist.")
+        if (! dir.isDirectory) throw NotDirectoryException("`${dir.absolutePath}` is not a directory.")
+
+        //init vars
+        this.dir = dir
+    }
+
+
+
+    companion object{
+        class DirectoryNotExist(message : String) : Exception(message)
+    }
 }
